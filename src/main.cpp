6b4a39c739469ec8b09314162097c15ba98c952c
Dmitriy <dmitsh7@gmail.com>
2023-08-14 21:09:50 +0400

    TRIANGLEgit add .!

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,51 @@
-
 #include <dinput.h>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+
+
+GLfloat point[] = {
+	0, 0.5f, 0,
+	0.5f, -0.5f, 0,
+	-0.5f, -0.5f, 0
+};
+
+GLfloat colors[] = {
+	1, 0, 0,
+	0, 1, 0,
+	0, 0, 1
+};
+const char* vertex_shader =
+"#version 460\n"
+"layout(location = 0) in vec3 vertex_pos;"
+"layout(location = 1) in vec3 vertex_col;"
+"out vec3 color;"
+"void main(){"
+"	color = vertex_col;"
+"	gl_Position = vec4(vertex_pos, 1.0);"
+"}";
+
+const char* fragment_shader = 
+"#version 460\n"
+"in vec3 color;"
+"out vec4 frag_color;"
+"void main(){"
+"	frag_color = vec4(color, 1.0);"
+"}";
 
 int g_width = 1600;
 int g_height = 1200;
 
 void glfwWindowSizeCallback(GLFWwindow* window, int width, int height)
 {
-    g_width = width;
-    g_height = height;
-    glViewport(0,0,g_width,g_height);
+	g_width = width;
+	g_height = height;
+	glViewport(0, 0, g_width, g_height);
 }
 
 void glfwKeyCallBack(GLFWwindow* window, int key, int scancode, int action, int mode)
 {
-	if(key == GLFW_KEY_SPACE && action == GLFW_PRESS)
+	if (key == GLFW_KEY_SPACE && action == GLFW_PRESS)
 	{
 		glfwSetWindowShouldClose(window, GL_TRUE);
 	}
@@ -34,7 +63,7 @@ int main(void)
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	GLFWwindow* window = glfwCreateWindow(g_width, g_height, "Engine", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(g_width, g_height, "Engine", nullptr, nullptr);
 	if (!window)
 	{
 		glfwTerminate();
@@ -59,13 +88,55 @@ int main(void)
 
 	glClearColor(1, 1, 0, 1);
 
+	GLuint vs = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vs, 1, &vertex_shader, nullptr);
+	glCompileShader(vs);
+
+	GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fs, 1, &fragment_shader, nullptr);
+	glCompileShader(fs);
+
+	GLuint shader_program  = glCreateProgram();
+	glAttachShader(shader_program, vs);
+	glAttachShader(shader_program, fs);
+	glLinkProgram(shader_program);
+
+	glDeleteShader(vs);
+	glDeleteShader(fs);
+
+	GLuint points_vbo;
+	glGenBuffers(1, &points_vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, points_vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(point), point, GL_STATIC_DRAW);
+
+	GLuint colors_vbo;
+	glGenBuffers(1, &colors_vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, colors_vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW);
+
+	GLuint vao;
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+
+	glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, points_vbo);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+
+	glEnableVertexAttribArray(1);
+	glBindBuffer(GL_ARRAY_BUFFER, colors_vbo);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+
+
 	while (!glfwWindowShouldClose(window))
 	{
 		glClear(GL_COLOR_BUFFER_BIT);
 
+		glUseProgram(shader_program);
+		glBindVertexArray(vao);
+		glDrawArrays(GL_TRIANGLES, 0, 3 );
+
 		glfwSwapBuffers(window);
 
-		/* Poll for and process events */
 		glfwPollEvents();
 	}
 
